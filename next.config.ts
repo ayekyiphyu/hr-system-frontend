1c9f72f90ad192a4586dc4bdaa45ebcfd3e554be
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-06-06 15:40:30 +0900

    fix:global.css edit and add to the shadcn UI input

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  webpack: (config) => {
+    config.resolve.alias["@components"] = path.join(__dirname, "components");
+    config.resolve.alias["@styles"] = path.join(__dirname, "styles");
+    return config;
+  },
 };
 
 export default nextConfig;
