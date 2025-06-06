14defc231f3681cea896cbde256ab5fdb3301da0
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-06-06 17:50:04 +0900

    fix:shadcn UI want to fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 
 
 import "@/styles/globals.css";
+
+import '@/lib/theme.config';
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,51 @@
-import theme from "./lib/theme.config";
 module.exports = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-        "./app/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./pages/**/*.{ts,tsx}",
+        "./styles/**/*.{css,scss}"
     ],
     theme: {
-        extend: theme.theme[0].extend,
-    },
+        extend: {
+            colors: {
+                primary: "#D6351A",
+                secondary: "#FFF8F8",
+                white: "#ffffff",
+                black: "#333333",
+                green: "#286E34",
+                yellow: "#FFEEAA",
+                blue: "#C8EDFD",
+                darkBlue: "#023F81",
+                red: "#df4759",
+                yellowLight: "#3498db",
+                greenLight: "#32ff7e",
+                navy: "#130f40",
+                orange: "#f39c12",
+            },
+            fontFamily: {
+                body: [
+                    "Hiragino Kaku Gothic Pro W3",
+                    "游ゴシック",
+                    "YuGothic",
+                    "ヒラギノ角ゴ ProN W3",
+                    "Hiragino Kaku Gothic ProN",
+                    "メイリオ",
+                    "Meiryo",
+                    "Helvetica Neue",
+                    "Helvetica",
+                    "Arial",
+                    "sans-serif",
+                ],
+            },
+            fontSize: {
+                fontError: "5rem",
+                fontHeader: "3rem",
+                fontTitle: "2rem",
+                fontLarge: "1.6rem",
+                fontRegular: "1.2rem",
+                fontSmall: "0.9rem",
+            },
+        },
+      },
     plugins: [],
 };
