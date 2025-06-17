ae35f7ba7a2368133fb6f11b283beb86d7d72f69
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-06-17 17:17:40 +0900

    feat:Add to the StaffPageLists

--- pages/staff/staff-lists/index.tsx
+++ pages/staff/staff-lists/index.tsx
@@ -1,18 +1,17 @@
-
 import Layout from "@/components/layout";
-import InvitationPage from "@/features/invitation/InvitationPage";
+import StaffListsPage from "@/features/stafflists/StaffListsPage";
 
-export default function StaffInvite() {
+export default function StaffLists() {
     return (
         <Layout
             adminName="花子 太郎"
             notificationCount={5}
         >
             <div className="container mx-auto sm:px-6 !p-0">
-                <InvitationPage />
+                <StaffListsPage />
             </div>
         </Layout>
 
+    )
 
-    );
 }
